6876a251e55943bef4632909407271c23c456ebd
mahdesignstudio <mahdesignstudio@gmail.com>
2020-06-05 23:34:37 -0400

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,7 +1,7 @@
-def my_each(collection)
+def my_each(collection) # define the my_each collection
   i = 0 
-  while i < collection.length 
-yield collection[i]
+  while i < collection.length # include while and iterate over each element
+yield collection[i] #yields the correct element adding a counter
   i = i+1 
 end 
 collection 
